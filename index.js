489e629fe82c109306f709168c95c33d553dac1e
GuilhemDeMarco <55885506+GuilhemDeMarco@users.noreply.github.com>
2020-01-22 10:31:22 +0100

    GET request handling and write score to db

--- index.js
+++ index.js
@@ -7,18 +7,46 @@ var jsonFile= require('jsonfile')
 //I am aware that it's not efficient, I will change this as soon as my code works
 var jsondb = "./db.json"
 
+console.log("up")
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
     var u = url.parse(req.url, true)
     var q = url.parse(req.url, true).query;
 
-    if(q){
-        console.log(q)
-        if(q.id && q.player && q.score){
-            console.log(q.id)
-            console.log(q.player)
-            console.log(q.score)
-
-        }
+    console.log(u.pathname)
+    if(u.pathname == "/scoring/"){
+      if(q){
+          console.log(q)
+          if(q.id && q.player && q.score){
+              console.log(q.id)
+              console.log(q.player)
+              console.log(q.score)
+              addToDB(q.id,q.player,q.score)
+          }
+      }
     }
 }).listen(8080);
+
+function addToDB(id,player,score){
+  console.log("add")
+  jsonFile.readFile(jsondb, function(err, data){
+    console.log(data)
+    console.log(data[id])
+    if(data[id]){
+      console.log("yup")
+      console.log(player)
+      let playerObject = {}
+      playerObject[player] = score
+      console.log(playerObject)
+      data[id].push(playerObject)
+      console.log(data[id])
+      //TODO : check if the player already has a score and change it if his new score is higher
+      jsonFile.writeFile(jsondb, data, { spaces: 2 }, function (err) {
+  			if (err) console.error(err)
+  		})
+    }
+    else{
+      console.log("nup")
+    }
+  })
+}
