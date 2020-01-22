dce9cf5f6b4eaead3fd2fe6391a3447ee845aceb
GuilhemDeMarco <55885506+GuilhemDeMarco@users.noreply.github.com>
2020-01-22 09:27:39 +0100

    comments and player parameter

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ var url = require('url')
 
 var jsonFile= require('jsonfile')
 
+//I'm using a json file as a database for now because I'm used to it
+//I am aware that it's not efficient, I will change this as soon as my code works
 var jsondb = "./db.json"
 
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
     var u = url.parse(req.url, true)
     var q = url.parse(req.url, true).query;
-    
+
     if(q){
         console.log(q)
-        if(q.id && q.score){
+        if(q.id && q.player && q.score){
             console.log(q.id)
+            console.log(q.player)
             console.log(q.score)
 
         }
